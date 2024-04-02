26791fca1e4d7466b4593885828d08a7cf00cbc6
Lee Theobald <Lee@LTheobald.co.uk>
2024-04-02 10:31:38 +0200

    Starting to add Kafka

---
+++ src/main/java/uk/co/ltheobald/trymsk/KafkaStack.java
@@ -0,0 +1,9 @@
+package uk.co.ltheobald.trymsk;
+
+import com.pulumi.aws.*;
+
+class KafkaStack extends Stack {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
