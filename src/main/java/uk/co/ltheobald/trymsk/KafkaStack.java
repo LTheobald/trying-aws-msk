b9f2c9d39deb9ea724f5d4422c6fd5db05cd1bd1
Lee Theobald <Lee@LTheobald.co.uk>
2024-04-06 16:31:59 +0000

    Fixing Pulumi issues

--- src/main/java/uk/co/ltheobald/trymsk/KafkaStack.java
+++ src/main/java/uk/co/ltheobald/trymsk/KafkaStack.java
@@ -1,11 +1,24 @@
 package uk.co.ltheobald.trymsk;
 
 import com.pulumi.Pulumi;
+import com.pulumi.Stack;
+import com.pulumi.awsnative.msk.Cluster;
+import com.pulumi.awsnative.msk.ClusterArgs;
+import com.pulumi.awsnative.msk.inputs.ClusterBrokerNodeGroupInfoArgs;
 
-class KafkaStack extends Stack {
-    public static void main(String[] args) {
+public class KafkaStack extends Stack {
+    public static void main(String... args) {
         Pulumi.run(ctx -> {
-            
+            Cluster kafkaCluster = new Cluster("my-msk-cluster",
+                    ClusterArgs.builder().brokerNodeGroupInfo(
+                            ClusterBrokerNodeGroupInfoArgs.builder()
+                                    .brokerAzDistribution("DEFAULT")
+                                    .instanceType("kafka.t5.small").build())
+                            .kafkaVersion("3.5.1")
+                            .numberOfBrokerNodes(3)
+                            .build());
+
+            ctx.export("kafkaClusterArn", kafkaCluster.arn());
         });
     }
 }
