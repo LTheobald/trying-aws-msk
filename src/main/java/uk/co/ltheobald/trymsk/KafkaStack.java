ac2d89a60e7f47ad87b1a003b328cf767f08c2e7
Lee Theobald <Lee@LTheobald.co.uk>
2024-04-06 21:13:22 +0000

    Simplifing

--- src/main/java/uk/co/ltheobald/trymsk/KafkaStack.java
+++ src/main/java/uk/co/ltheobald/trymsk/KafkaStack.java
@@ -1,43 +1,55 @@
 package uk.co.ltheobald.trymsk;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.pulumi.Pulumi;
 import com.pulumi.awsnative.ec2.Subnet;
 import com.pulumi.awsnative.ec2.SubnetArgs;
 import com.pulumi.awsnative.ec2.Vpc;
+import com.pulumi.awsnative.ec2.VpcArgs;
 import com.pulumi.awsnative.msk.Cluster;
 import com.pulumi.awsnative.msk.ClusterArgs;
 import com.pulumi.awsnative.msk.inputs.ClusterBrokerNodeGroupInfoArgs;
-import com.pulumi.resources.CustomResourceOptions;
-import com.pulumi.resources.ResourceOptions;
 
 public class KafkaStack {
-    private static final String VPC_ID = "vpc";
+    // TODO Fetch from the config
     private static final String AWS_REGION = "eu-west-1";
 
     public static void main(String... args) {
         Pulumi.run(ctx -> {
-            Vpc vpc = new Vpc(VPC_ID);
-            Subnet subnetA = new Subnet("subnetA",
-                    SubnetArgs.builder().availabilityZone(AWS_REGION + "a").vpcId(VPC_ID).build(),
-                    CustomResourceOptions.builder().dependsOn(vpc).build());
-            Subnet subnetB = new Subnet("subnetB",
-                    SubnetArgs.builder().availabilityZone(AWS_REGION + "b").vpcId(VPC_ID).build(),
-                    CustomResourceOptions.builder().dependsOn(vpc).build());
-            Subnet subnetC = new Subnet("subnetC",
-                    SubnetArgs.builder().availabilityZone(AWS_REGION + "c").vpcId(VPC_ID).build(),
-                    CustomResourceOptions.builder().dependsOn(vpc).build());
+            Vpc vpc = new Vpc("vpc", VpcArgs.builder().cidrBlock("10.0.0.0/16").build());
+            
+            // Create 3 Subnets for the VPC
+            List<String> availabilityZones = Arrays.asList(AWS_REGION+"a", AWS_REGION+"b", AWS_REGION+"c");
+            List<Subnet> subnets = new ArrayList<>();
+            for (int i = 0; i < 3; i++) {
+                subnets.add(new Subnet(String.format("my-subnet-%d", i), SubnetArgs.builder()
+                    .vpcId(vpc.id())
+                    .cidrBlock(String.format("10.0.%d.0/24", i))
+                    .availabilityZone(availabilityZones.get(i))
+                    .build()));git add .
+                    clear
+                    
+            }
+            List<String> subnetIds = subnets.stream()
+                .map(subnet -> subnet.id().toString())
+                .collect(Collectors.toList());
 
             Cluster kafkaCluster = new Cluster("my-msk-cluster",
                     ClusterArgs.builder().brokerNodeGroupInfo(
                             ClusterBrokerNodeGroupInfoArgs.builder()
                                     .brokerAzDistribution("DEFAULT")
-                                    .clientSubnets(subnetA.id().toString(), subnetB.id().toString(), subnetC.id().toString())
+                                    .clientSubnets(subnetIds)
                                     .instanceType("kafka.t5.small").build())
                             .kafkaVersion("3.5.1")
                             .numberOfBrokerNodes(3)
                             .build());
 
             ctx.export("kafkaClusterArn", kafkaCluster.arn());
+            ctx.export("kafkaClusterName", kafkaCluster.clusterName());
         });
     }
 }
