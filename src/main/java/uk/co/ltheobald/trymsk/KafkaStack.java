d51bb35e34fd93104b83a069a5df7b3df42628bc
Lee Theobald <Lee@LTheobald.co.uk>
2024-04-06 20:55:14 +0000

    Now (hopefully) getting near a working cluster

--- src/main/java/uk/co/ltheobald/trymsk/KafkaStack.java
+++ src/main/java/uk/co/ltheobald/trymsk/KafkaStack.java
@@ -1,17 +1,37 @@
 package uk.co.ltheobald.trymsk;
 
 import com.pulumi.Pulumi;
+import com.pulumi.awsnative.ec2.Subnet;
+import com.pulumi.awsnative.ec2.SubnetArgs;
+import com.pulumi.awsnative.ec2.Vpc;
 import com.pulumi.awsnative.msk.Cluster;
 import com.pulumi.awsnative.msk.ClusterArgs;
 import com.pulumi.awsnative.msk.inputs.ClusterBrokerNodeGroupInfoArgs;
+import com.pulumi.resources.CustomResourceOptions;
+import com.pulumi.resources.ResourceOptions;
 
 public class KafkaStack {
+    private static final String VPC_ID = "vpc";
+    private static final String AWS_REGION = "eu-west-1";
+
     public static void main(String... args) {
         Pulumi.run(ctx -> {
+            Vpc vpc = new Vpc(VPC_ID);
+            Subnet subnetA = new Subnet("subnetA",
+                    SubnetArgs.builder().availabilityZone(AWS_REGION + "a").vpcId(VPC_ID).build(),
+                    CustomResourceOptions.builder().dependsOn(vpc).build());
+            Subnet subnetB = new Subnet("subnetB",
+                    SubnetArgs.builder().availabilityZone(AWS_REGION + "b").vpcId(VPC_ID).build(),
+                    CustomResourceOptions.builder().dependsOn(vpc).build());
+            Subnet subnetC = new Subnet("subnetC",
+                    SubnetArgs.builder().availabilityZone(AWS_REGION + "c").vpcId(VPC_ID).build(),
+                    CustomResourceOptions.builder().dependsOn(vpc).build());
+
             Cluster kafkaCluster = new Cluster("my-msk-cluster",
                     ClusterArgs.builder().brokerNodeGroupInfo(
                             ClusterBrokerNodeGroupInfoArgs.builder()
                                     .brokerAzDistribution("DEFAULT")
+                                    .clientSubnets(subnetA.id().toString(), subnetB.id().toString(), subnetC.id().toString())
                                     .instanceType("kafka.t5.small").build())
                             .kafkaVersion("3.5.1")
                             .numberOfBrokerNodes(3)
