1ada1757a0dc03da8d6c1062f99a5d8d94bd1fa2
Lee Theobald <Lee@LTheobald.co.uk>
2024-04-06 10:46:39 +0000

    Tinkering

--- src/main/java/uk/co/ltheobald/trymsk/KafkaStack.java
+++ src/main/java/uk/co/ltheobald/trymsk/KafkaStack.java
@@ -1,9 +1,20 @@
 package uk.co.ltheobald.trymsk;
 
-import com.pulumi.aws.*;
+import com.pulumi.awsnative.*;
+import com.pulumi.awsnative.msk.*;
 
 class KafkaStack extends Stack {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+         Cluster kafkaCluster = new Cluster("my-kafka-cluster", new ClusterArgs()
+            .setBrokerNodeGroupInfo(new ClusterArgs.BrokerNodeGroupInfoArgs()
+                .setAzDistribution("DEFAULT")
+                .setInstanceType("kafka.t3.small")
+                .setEbsVolumeSize(1))
+            .setKafkaVersion("3.5.1")
+            .setNumberOfBrokerNodes(3));
+
+        this.export("kafkaClusterArn", kafkaCluster.getArn());
+        this.export("kafkaClusterZookeeperConnectString", kafkaCluster.getZookeeperConnectString());
+    }
     }
 }
